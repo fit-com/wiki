0450786ec6d9c528c94f6e839d7d7986cf5b060f
fit-com <nsufitcommunity@proton.me>
2026-07-25 01:22:15 +0700

    Register Mermaid component in VitePress theme

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import Mermaid from './Mermaid.vue'
 import './custom.css'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('Mermaid', Mermaid)
+  }
 }
+
