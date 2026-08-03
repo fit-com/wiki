5a4dc58c5d29dab13d4ca9aa3408ec9156acca3e
fit-com <nsufitcommunity@proton.me>
2026-08-04 00:51:53 +0700

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 import './custom.css'
 
 export default {
   extends: Layout,
-  enhanceApp({ app }) {
-    // Ваш код, если есть
-  }
+  enhanceApp({ app }) { }
 }
