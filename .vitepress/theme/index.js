408b46c3275145e35b6c3d2cc27987694ba03db3
fit-com <nsufitcommunity@proton.me>
2026-08-04 00:34:43 +0700

    Add custom app enhancement to VitePress theme
    Enhance the VitePress theme by adding custom logic through a mixin.

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import injectCategories from './injectCategories.js'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    // Добавляем кастомную логику
+    app.mixin(injectCategories)
+  }
 }
-
