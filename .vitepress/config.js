c88e755aa6e012fcfd4e7891732b8a1dbb94a835
fit-com <nsufitcommunity@proton.me>
2026-08-04 20:23:21 +0700

    Remove sidebar configuration from config.js
    Removed sidebar configuration from VitePress config.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -16,20 +16,6 @@ export default {
       provider: 'local'
     },
 
-    // Настраиваем левое меню (Sidebar)
-    sidebar: [
-      {
-        text: 'Категории',
-        items: [
-          { text: '👨‍🏫 Преподаватели', link: '/teachers/'  },
-          { text: '🎭 События' },
-          { text: '🛠️ Предметы' },
-          { text: '🏢 Корпуса и аудитории' },
-          { text: '➕ Добавить контент', link: 'https://github.com/fit-com/wiki/new/main' }
-        ]
-      }
-    ],
-
     editLink: {
       pattern: 'https://github.com/fit-com/wiki/edit/main/:path',
       text: 'Редактировать статью на GitHub'
